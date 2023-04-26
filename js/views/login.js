8945e300d5af6f81f98ce7bfd36466429475c9af
Prof. Ms. Ricardo Leme <ricardo.leme@gmail.com>
2023-04-26 09:35:23 -0300

    change user and email const

--- js/views/login.js
+++ js/views/login.js
@@ -19,12 +19,12 @@
  // Atalhos para os elementos DOM - Document Object Model
 const formLogin = document.getElementById('formLogin')
 const botaoGoogle = document.getElementById('loginGoogle')
-const email = document.getElementById('email').value
-const senha = document.getElementById('senha').value
 
 //Adiciona um Listener em cada item
 formLogin.addEventListener('submit', (event) => {
   event.preventDefault()
+  const email = document.getElementById('email').value
+  const senha = document.getElementById('senha').value
   loginFirebase(email, senha)
 }
 )
